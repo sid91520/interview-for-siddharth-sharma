7f03e99a036541edcca5493197161e87147e8f5d
siddharth91520 <sidharth@retire100.live.com>
2025-07-05 16:10:46 +0530

    added loader and filter by upcoming or successful launches

---
+++ src/context/filterContext.js
@@ -0,0 +1,11 @@
+import { useState,createContext  } from "react";
+
+export const UserContext = createContext();
+export function UserProvider({ children }) {
+  const [filter, setFilter] = useState("all");
+  return (
+    <UserContext.Provider value={{ filter, setFilter }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
